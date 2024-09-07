4a429b0447ab40497c3d1d1f6a24187deeb6d970
Spring-Mirage <spring-mirage>
2024-09-06 20:34:50 -0500

    update

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true,
+    es2020: true, 
+    node: true, // Daisy Ui error
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -17,5 +21,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    //no react use error
+    'no-unused-vars': 0,
   },
 }
---
+++ client/src/layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+        <Navbar />
+        <Outlet />
+        <footer>Footer</footer>
+    </div>
+  )
+};
+
+export default Main;
+
